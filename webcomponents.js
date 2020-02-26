291db16d81d608207ab1423e2d78f9e8a2af88f3
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2020-02-26 15:02:53 +0100

    ;

--- webcomponents.js
+++ webcomponents.js
@@ -168,7 +168,7 @@
 
 			//needleWaypoints is defined with positive y axis being up
 			//The initial definition of needleWaypoints is for a full diamond, but if this._enableIndicatorNeedleTail is false, we'll abbreviate to a chevron
-			needleWaypoints = [{x: 0,y: this._needleHeadLength}, {x: needleWaypointOffset,y: 0}, {x: (-1*needleWaypointOffset),y: 0}, {x: 0,y: this._needleHeadLength}]
+			needleWaypoints = [{x: 0,y: this._needleHeadLength}, {x: needleWaypointOffset,y: 0}, {x: (-1*needleWaypointOffset),y: 0}, {x: 0,y: this._needleHeadLength}];
 
 			//we need to invert the y-axis and scale the indicator to the gauge.
 			//  If Y = 100, then that is 100% of outer radius.  So of Y = 100 and outerRad = 70, then the scaled Y will be 70.
